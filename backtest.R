f9ca5e00fe926170a3e05ba451d0b033c896da79
yepengxj <yepeng_xj@163.com>
2015-03-03 16:44:15 +0800

    new file:   backtest.R 	new file:   pred_000001.R

---
+++ backtest.R
@@ -0,0 +1,215 @@
+library(EMD)
+library(quantstrat)
+library(dtw)
+library(plyr)
+library("e1071")
+library("hht")
+
+xxx<-read.csv("~/temp/0000300.csv",header=F,skip=1,encoding = "UTF-8")
+xxx<-xts(xxx[,c(4,5,6,7,12)],order.by=as.Date(xxx[,1],"%Y-%m-%d"))
+colnames(xxx)<-c("close","high","low","open","vol")
+x<-"/2015-01-01"
+pres_xxx_ts[x]
+backtest_2009_07_01_2014_07_01<-backtest_func("2009-07-01/2014-07-01",xxx)
+backtest_2014_07_01<-backtest_func("2014-07-01/",xxx)
+
+
+x<-"pred_resRlibeemd_eemd_func_get_acf1_get_k_noise_t1_smooth1"
+date_range<-"2015-02-01/"
+backtest_func<-function(date_range,xxx){
+  
+  aaply(list.files("~/temp/",pattern="^pred_resRlibeemd_eemd"),1,function(x,xxx,date_range){
+    
+    filepath<-paste("~/temp/",x,sep="")
+    pred_xxx<-read.csv(filepath,header=T,encoding="UTF-8")
+    
+    pres_xxx_ts<-xts(pred_xxx[,-1],order.by=as.Date(pred_xxx[,1],origin = "1970-01-01"))
+    pred_close_price<-merge(pres_xxx_ts[date_range],xxx,join="left")
+    pred_close_price$sell_signal<-((pred_close_price[,1]>pred_close_price[,2])&(pred_close_price[,2]>pred_close_price[,3]))
+    pred_close_price$buy_signal<-((pred_close_price[,1]<pred_close_price[,2])&(pred_close_price[,2]<pred_close_price[,3]))
+    pred_close_price$close<-pred_close_price$close/1000
+    pred_close_price$high<-pred_close_price$high/1000
+    pred_close_price$low<-pred_close_price$low/1000
+    pred_close_price$open<-pred_close_price$open/1000
+    print(head(pred_close_price,4))
+    
+    currency("RMB")
+    try(rm("order_book.qFaber",pos=.strategy),silent=TRUE)
+    try(rm("account.qFaber","portfolio.qFaber",pos=.blotter),silent=TRUE)
+    ## [1] "RMB"
+    stock("ZSYH", currency = "RMB", multiplier = 1)
+    ## [1] "ZSYH"
+    # 设定时区
+    Sys.setenv(TZ = "GMT+8")
+    ZSYH<-pred_close_price
+    print(head(ZSYH,4))
+    # 初始化组合和账户
+    q.strategy <- "qFaber"
+    initPortf(q.strategy, "ZSYH", initDate = "2002-01-31")
+    ## [1] "qFaber"
+    initAcct(q.strategy, portfolios = q.strategy, initDate = "2002-01-31", initEq = 100000)
+    ## [1] "qFaber"
+    # 初始化指定和策略
+    initOrders(portfolio = q.strategy, initDate = "2002-01-31")
+    
+    add.signal(q.strategy,name="sigThreshold",
+               arguments = list(column="buy_signal",
+                                relationship="eq",
+                                threshold=1,
+                                cross=TRUE),
+               label="buy_signal_add"
+    )
+    
+    add.signal(q.strategy,name="sigThreshold",
+               arguments = list(column="sell_signal",
+                                relationship="eq",
+                                threshold=1),
+               label="sell_signal_add"
+    )
+    
+    
+    add.rule(q.strategy, name = "ruleSignal", arguments = list(sigcol = "buy_signal_add", 
+                                                               sigval = TRUE, orderqty = 50000, ordertype = "market", orderside = "long", 
+                                                               pricemethod = "market"), type = "enter", path.dep = TRUE) # 买入数量为900股
+    
+    add.rule(q.strategy, name = "ruleSignal", arguments = list(sigcol = "sell_signal_add", 
+                                                               sigval = TRUE, orderqty = "all", ordertype = "market", orderside = "long", 
+                                                               pricemethod = "market"), type = "exit", path.dep = TRUE)
+    
+    applyStrategy(strategy = q.strategy, portfolios = q.strategy,mktdata=pred_close_price)
+    
+    
+    updatePortf(q.strategy)
+    
+    updateAcct(q.strategy)
+    
+    updateEndEq(q.strategy)
+    
+    myTheme <- chart_theme()
+    myTheme$col$dn.col <- "lightgreen"
+    myTheme$col$up.col <- "lightblue"
+    myTheme$col$dn.border <- "grey"
+    myTheme$col$up.border <- "grey"
+    
+    #chart.Posn(q.strategy, Symbol = "ZSYH", Dates = date_range, theme=myTheme)
+    #(tstats <- tradeStats(Portfolio = q.strategy, Symbol = "ZSYH"))
+    tradeStats_def<-data.frame("Portfolio"=q.strategy,"Symbol"="ZSYH",
+                         "Num.Txns"=0,
+                         "Num.Trades"=0,
+                         "Net.Trading.PL"=0,
+                         "Avg.Trade.PL"=0,
+                         "Med.Trade.PL"=0,
+                         "Largest.Winner"=0,
+                         "Largest.Loser"=0,
+                         "Gross.Profits"=0,
+                         "Gross.Losses"=0,
+                         "Std.Dev.Trade.PL"=0,
+                         "Percent.Positive"=0,
+                         "Percent.Negative"=0,
+                         "Profit.Factor"=0,
+                         "Avg.Win.Trade"=0,
+                         "Med.Win.Trade"=0,
+                         "Avg.Losing.Trade"=0,
+                         "Med.Losing.Trade"=0,
+                         "Avg.Daily.PL"=0,
+                         "Med.Daily.PL"=0,
+                         "Std.Dev.Daily.PL"=0,
+                         "Ann.Sharpe"=0,
+                         "Max.Drawdown"=0,
+                         "Profit.To.Max.Draw"=0,
+                         "Avg.WinLoss.Ratio"=0,
+                         "Med.WinLoss.Ratio"=0,
+                         "Max.Equity"=0,
+                         "Min.Equity"=0,
+                         "End.Equity"=0)
+    
+    
+    
+    ifelse(is.null(tradeStats(Portfolio = q.strategy, Symbol = "ZSYH")),
+                   trades<-tradeStats_def,
+                   trades<-tradeStats(Portfolio = q.strategy, Symbol = "ZSYH"))
+    trades$name<-x
+    print(trades)
+    
+    trades
+   # data.frame("name"=x,
+  #       ifelse(is.null(tradeStats(Portfolio = q.strategy, Symbol = "ZSYH")),
+   #             tradeStats_def,
+    #            tradeStats(Portfolio = q.strategy, Symbol = "ZSYH")))
+  },xxx,date_range)
+  
+}  
+  
+
+
+library(quantstrat)
+# 金融产品初始化
+currency("RMB")
+q.strategy <- "qFaber"
+try(rm("order_book.qFaber",pos=.strategy),silent=TRUE)
+try(rm("account.qFaber","portfolio.qFaber",pos=.blotter),silent=TRUE)
+## [1] "RMB"
+stock("ZSYH", currency = "RMB", multiplier = 1)
+## [1] "ZSYH"
+# 设定时区
+Sys.setenv(TZ = "GMT+8")
+ZSYH<-pred_close_price
+# 初始化组合和账户
+q.strategy <- "qFaber"
+initPortf(q.strategy, "ZSYH", initDate = "2002-01-31")
+## [1] "qFaber"
+initAcct(q.strategy, portfolios = q.strategy, initDate = "2002-01-31", initEq = 100000)
+## [1] "qFaber"
+# 初始化指定和策略
+initOrders(portfolio = q.strategy, initDate = "2002-01-31")
+strategy(q.strategy, store = TRUE)
+
+add.signal(q.strategy,name="sigThreshold",
+           arguments = list(column="buy_signal",
+                            relationship="eq",
+                            threshold=1,
+                            cross=TRUE),
+           label="buy_signal_add"
+)
+
+add.signal(q.strategy,name="sigThreshold",
+           arguments = list(column="sell_signal",
+                            relationship="eq",
+                            threshold=1),
+           label="sell_signal_add"
+)
+
+
+add.rule(q.strategy, name = "ruleSignal", arguments = list(sigcol = "buy_signal_add", 
+                                                           sigval = TRUE, orderqty = 50000, ordertype = "market", orderside = "long", 
+                                                           pricemethod = "market"), type = "enter", path.dep = TRUE) # 买入数量为900股
+
+add.rule(q.strategy, name = "ruleSignal", arguments = list(sigcol = "sell_signal_add", 
+                                                           sigval = TRUE, orderqty = "all", ordertype = "market", orderside = "long", 
+                                                           pricemethod = "market"), type = "exit", path.dep = TRUE)
+
+
+applyStrategy(strategy = q.strategy, portfolios = q.strategy)
+
+
+updatePortf(q.strategy)
+
+updateAcct(q.strategy)
+
+updateEndEq(q.strategy)
+
+myTheme <- chart_theme()
+myTheme$col$dn.col <- "lightgreen"
+myTheme$col$up.col <- "lightblue"
+myTheme$col$dn.border <- "grey"
+myTheme$col$up.border <- "grey"
+
+chart.Posn(q.strategy, Symbol = "ZSYH", Dates = paste('2008-01-01/','2015-01-30',sep=''), theme=myTheme)
+(tstats <- tradeStats(Portfolio = q.strategy, Symbol = "ZSYH"))
+
+
+ob <- getOrderBook(q.strategy)
+head(ob$qFaber$ZSYH)
+
+chart.ME(Portfolio = q.strategy, Symbol = "ZSYH", type = "MAE", scale = "percent")
+chart.ME(Portfolio = q.strategy, Symbol = "ZSYH", type = "MFE", scale = "percent")
---
+++ pred_000001.R
@@ -0,0 +1,370 @@
+library(EMD)
+library(quantstrat)
+library(dtw)
+library(plyr)
+library("e1071")
+library("hht")
+
+
+#####准备数据from tdx
+HS300_fund<-read.table('~/Downloads/SZ159919.txt',header=F,sep="\t",skip = 2,fill=T)
+HS300_fund<-HS300_fund[-nrow(HS300_fund),]
+HS300_fund_ts<-xts(HS300_fund[,-1],order.by=as.Date(HS300_fund[,1],format="%m/%d/%Y",origin = "1970-01-01"))
+colnames(HS300_fund_ts)<-c("open","high","low","close","vol","amount")
+
+HS300_idx<-read.table('~/Downloads/SZ399300.txt',header=F,sep="\t",skip = 2,fill=T)
+HS300_idx<-HS300_idx[-nrow(HS300_idx),]
+HS300_idx_ts<-xts(HS300_idx[,-1],order.by=as.Date(HS300_idx[,1],format="%m/%d/%Y",origin = "1970-01-01"))
+colnames(HS300_idx_ts)<-c("open","high","low","close","vol","amount")
+
+
+
+####sma5降噪
+HS300_idx_ts$sma5_close<-SMA(HS300_idx_ts[,"close"],5)
+
+#ac预测
+result_data_hs300_sma5_smooth<-aaply(as.numeric(.indexDate(HS300_idx_ts["2011-01-01/2015-01-19","sma5_close"])), 1, 
+                                    function(x,ts,i,j){
+                                      start_date_id<-which(.indexDate(ts)==x)
+                                      end_date<-.indexDate(ts)[(start_date_id+i-1)]
+                                      if(!is.na(end_date))
+                                      {
+                                        start_end_str<-paste(as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),
+                                                             "/",
+                                                             as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),
+                                                             sep="")
+                                        start_str<-paste("/",as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+                                        print(start_end_str)
+                                        print(start_str)
+                                        ac_indicator(ts[start_end_str],ts[start_str],i,j)
+                                      }
+                                      
+                                    },HS300_idx_ts["2006-01-01/","sma5_close"],10,3)
+
+
+####ema5降噪
+HS300_idx_ts$ema5_close<-EMA(HS300_idx_ts[,"close"],5)
+
+result_data_hs300_ema5_smooth<-aaply(as.numeric(.indexDate(HS300_idx_ts["2011-01-01/2015-01-19","ema5_close"])), 1, 
+                                     function(x,ts,i,j){
+                                       start_date_id<-which(.indexDate(ts)==x)
+                                       end_date<-.indexDate(ts)[(start_date_id+i-1)]
+                                       if(!is.na(end_date))
+                                       {
+                                         start_end_str<-paste(as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),
+                                                              "/",
+                                                              as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),
+                                                              sep="")
+                                         start_str<-paste("/",as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+                                         print(start_end_str)
+                                         print(start_str)
+                                         ac_indicator(ts[start_end_str],ts[start_str],i,j)
+                                       }
+                                       
+                                     },HS300_idx_ts["2006-01-01/","ema5_close"],10,3)
+
+#####emd降噪
+HS300_idx_ts$close_emd_smooth<-emd_smooth(as.numeric(HS300_idx_ts[,"close"]))
+
+#ac预测   2009-01-16/2015-01-30
+result_data_hs300_emd_smooth<-aaply(as.numeric(.indexDate(HS300_idx_ts["2009-01-01/2015-01-19","close"])), 1, 
+                       function(x,ts,i,j){
+                         start_date_id<-which(.indexDate(ts)==x)
+                         end_date<-.indexDate(ts)[(start_date_id+i-1)]
+                         start_end_str<-paste(as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),
+                                              "/",
+                                              as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),
+                                              sep="")
+                         start_str<-paste("/",as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+                         end_str<-paste("/",as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+                         #print(start_end_str)
+                         #print(start_str)
+                         #print(end_str)
+                         ts_curr<-ts[end_str]
+                         ts_curr$emd_smmoth<-emd_smooth(as.numeric(ts[end_str]))
+                         ac_indicator(ts_curr[start_end_str,"emd_smmoth"],ts_curr[start_str,"emd_smmoth"],i,j)
+                         
+                       },HS300_idx_ts["2006-01-01/","close"],10,3)
+
+#####EEMD降噪
+HS300_idx_ts$close_eemd_smooth<-eemd_smooth(as.numeric(HS300_idx_ts[,"close"]))
+
+#ac预测2009-02-06  2008-01-15/2015-01-30
+result_data_hs300_eemd_smooth<-aaply(as.numeric(.indexDate(HS300_idx_ts["2008-01-01/2015-01-19","close"])), 1, 
+                                    function(x,ts,i,j){
+                                      start_date_id<-which(.indexDate(ts)==x)
+                                      end_date<-.indexDate(ts)[(start_date_id+i-1)]
+                                      if(!is.na(end_date))
+                                      {
+                                        start_end_str<-paste(as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),
+                                                             "/",
+                                                             as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),
+                                                             sep="")
+                                        start_str<-paste("/",as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+                                        end_str<-paste("/",as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+                                        #print(start_end_str)
+                                        #print(start_str)
+                                        #print(end_str)
+                                        ts_curr<-ts[end_str]
+                                        ts_curr$eemd_smmoth<-eemd_smooth(as.numeric(ts[end_str]))
+                                        ac_indicator(ts_curr[start_end_str,"eemd_smmoth"],ts_curr[start_str,"eemd_smmoth"],i,j)
+                                      }
+                                      
+                                    },HS300_idx_ts["2000-01-01/","close"],10,3)
+
+pred_result_ts<-as.xts(result_data_hs300_eemd_smooth,order.by=time(HS300_idx_ts["2009-02-06/2015-02-16"]))
+#####SVR
+###ema15降噪
+HS300_idx_ts$ema15_close<-EMA(HS300_idx_ts[,"close"],15)
+HS300_idx_ts$ema15_last1day_close<-xts_shitf_days(HS300_idx_ts[,"ema15_close"],-1)
+HS300_idx_ts$ema15_last2day_close<-xts_shitf_days(HS300_idx_ts[,"ema15_close"],-2)
+HS300_idx_ts$ema15_last3day_close<-xts_shitf_days(HS300_idx_ts[,"ema15_close"],-3)
+
+###ema10 20 3
+HS300_idx_ts$ema10_close<-EMA(HS300_idx_ts[,"close"],10)
+HS300_idx_ts$ema20_close<-EMA(HS300_idx_ts[,"close"],20)
+HS300_idx_ts$ema3_close<-EMA(HS300_idx_ts[,"close"],3)
+HS300_idx_ts$ema3_next5day_close<-xts_shitf_days(HS300_idx_ts[,"ema3_close"],5)
+HS300_idx_ts$ema3_next5day_roc<-(HS300_idx_ts[,"ema3_next5day_close"]-HS300_idx_ts[,"ema3_close"])/HS300_idx_ts[,"ema3_close"]
+
+
+
+###5日收益率
+HS300_idx_ts$roc5_close<-ROC(HS300_idx_ts[,"close"],5)
+HS300_idx_ts$roc5_last1day_close<-xts_shitf_days(HS300_idx_ts[,"roc5_close"],-1)
+HS300_idx_ts$roc5_last2day_close<-xts_shitf_days(HS300_idx_ts[,"roc5_close"],-2)
+HS300_idx_ts$roc5_last3day_close<-xts_shitf_days(HS300_idx_ts[,"roc5_close"],-3)
+HS300_idx_ts$roc5_last4day_close<-xts_shitf_days(HS300_idx_ts[,"roc5_close"],-4)
+
+
+
+###10日收益率
+HS300_idx_ts$roc10_close<-ROC(HS300_idx_ts[,"close"],10)
+HS300_idx_ts$roc10_last1day_close<-xts_shitf_days(HS300_idx_ts[,"roc10_close"],-1)
+HS300_idx_ts$roc10_last2day_close<-xts_shitf_days(HS300_idx_ts[,"roc10_close"],-2)
+HS300_idx_ts$roc10_last3day_close<-xts_shitf_days(HS300_idx_ts[,"roc10_close"],-3)
+HS300_idx_ts$roc10_last4day_close<-xts_shitf_days(HS300_idx_ts[,"roc10_close"],-4)
+
+###15日收益率
+HS300_idx_ts$roc15_close<-ROC(HS300_idx_ts[,"close"],15)
+HS300_idx_ts$roc15_last1day_close<-xts_shitf_days(HS300_idx_ts[,"roc15_close"],-1)
+HS300_idx_ts$roc15_last2day_close<-xts_shitf_days(HS300_idx_ts[,"roc15_close"],-2)
+HS300_idx_ts$roc15_last3day_close<-xts_shitf_days(HS300_idx_ts[,"roc15_close"],-3)
+HS300_idx_ts$roc15_last4day_close<-xts_shitf_days(HS300_idx_ts[,"roc15_close"],-4)
+
+###20日收益率
+HS300_idx_ts$roc20_close<-ROC(HS300_idx_ts[,"close"],20)
+HS300_idx_ts$roc20_last1day_close<-xts_shitf_days(HS300_idx_ts[,"roc20_close"],-1)
+HS300_idx_ts$roc20_last2day_close<-xts_shitf_days(HS300_idx_ts[,"roc20_close"],-2)
+HS300_idx_ts$roc20_last3day_close<-xts_shitf_days(HS300_idx_ts[,"roc20_close"],-3)
+HS300_idx_ts$roc20_last4day_close<-xts_shitf_days(HS300_idx_ts[,"roc20_close"],-4)
+
+##OBV
+HS300_idx_ts$obv<-OBV(HS300_idx_ts[,"close"],HS300_idx_ts[,"vol"])
+
+##log2high_low
+HS300_idx_ts$log2high_low5<-sqrt(runSum(log2(HS300_idx_ts[,"high"]/HS300_idx_ts[,"low"]),5)/5)*80
+
+
+##SVM
+kernel_type <- "radial"
+max_gamma <- -2
+min_gamma <- -6
+max_cost <- 3
+min_cost <- -6
+
+tuned <- tune.svm(survival_days ~ ., data = train_set,
+                  kernel = kernel_type,
+                  gamma = 10^(min_gamma:max_gamma),
+                  cost = 10^(min_cost:max_cost))
+tuned<-tune.svm(HS300_idx_ts["2015-01-15/2015-02-06",
+           c("ema15_close", "ema15_last1day_close","ema15_last2day_close",
+             "ema15_last3day_close", "ema10_close", "ema20_close","ema3_close",
+              "roc5_close", "roc5_last1day_close", "roc5_last2day_close",
+             "roc5_last3day_close", "roc5_last4day_close", "roc10_close", "roc10_last1day_close", 
+             "roc10_last2day_close", "roc10_last3day_close", "roc10_last4day_close", "roc15_close", 
+             "roc15_last1day_close", "roc15_last2day_close", "roc15_last3day_close", "roc15_last4day_close", 
+             "roc20_close", "roc20_last1day_close", "roc20_last2day_close", "roc20_last3day_close", 
+             "roc20_last4day_close", "obv", "log2high_low5")],
+           HS300_idx_ts["2015-01-15/2015-02-06", "ema3_next5day_roc"],
+           kernel_type="radial",
+           gamma = 10^(min_gamma:max_gamma),
+           cost = 10^(min_cost:max_cost))
+               
+predict_data<-predict(svm_model, HS300_idx_ts["2015-02-09",
+                                c("ema15_close", "ema15_last1day_close","ema15_last2day_close",
+                                  "ema15_last3day_close", "ema10_close", "ema20_close","ema3_close",
+                                  "roc5_close", "roc5_last1day_close", "roc5_last2day_close",
+                                  "roc5_last3day_close", "roc5_last4day_close", "roc10_close", "roc10_last1day_close", 
+                                  "roc10_last2day_close", "roc10_last3day_close", "roc10_last4day_close", "roc15_close", 
+                                  "roc15_last1day_close", "roc15_last2day_close", "roc15_last3day_close", "roc15_last4day_close", 
+                                  "roc20_close", "roc20_last1day_close", "roc20_last2day_close", "roc20_last3day_close", 
+                                  "roc20_last4day_close", "obv", "log2high_low5")])
+
+
+xts_shitf_days<-function(xts_data,shitf_days)
+{
+  if(shitf_days>0)
+  {
+    new_idx_date<-.indexDate(xts_data)[1:(length(.indexDate(xts_data))-shitf_days)]
+    
+    shitf_data<-as.data.frame(xts_data)[(1+shitf_days):length(.indexDate(xts_data)),]
+    
+    xts(shitf_data,order.by=(as.Date(new_idx_date,format="%Y-%m-%d",origin = "1970-01-01")))
+    
+  }
+  else
+  {
+    new_idx_date<-.indexDate(xts_data)
+    shitf_data<-as.data.frame(xts_data)
+    dim_data<-dim(shitf_data)
+    for(i in (1:abs(shitf_days)))
+    {
+      shitf_data<-rbind(rep(NA,dim_data[2]),shitf_data)
+    }
+    
+    xts(shitf_data[1:length(new_idx_date),],order.by=(as.Date(new_idx_date,format="%Y-%m-%d",origin = "1970-01-01")))
+    
+  }
+  
+}
+
+#=====EEMD趋势拟合测试
+
+#归一化
+normalize_func<-function(x)
+{
+  (x-min(x))/(max(x)-min(x))+1
+}
+
+denormalize_func<-function(x,max_x,min_x)
+{
+  (x-1)*(max_x-min_x)+min_x
+}
+
+# Decompose with EEMD
+imfs <- Rlibeemd::eemd(normalize_func(xxx$close), num_siftings = 1000, ensemble_size = 100, threads = 2)
+head(imfs,10)
+plot(imfs[,c(1:5)])
+plot(imfs[,c(6:11)])
+# High frequencies
+ts.plot(rowSums(imfs[,1:3]))
+# Low frequencies
+ts.plot(rowSums(imfs[,4:ncol(imfs)]))
+
+test<-HS300_idx_ts["2006-01-01/","close_emd_smooth"]
+test[!is.na(test[,1]),]
+testdata<-close_emd_smooth_ts
+
+testdata<-SMA(xxx[,"close"],5)
+testdata<-testdata[!is.na(testdata)]
+
+plot(testdata["2014-01-01/2015-01-30",1],ylim=c(min(testdata["2014-01-01/2015-01-30",1],na.rm=T),max(testdata["2014-01-01/2015-01-30",1],na.rm=T)))
+
+which(.indexDate(testdata["2011-01-01/2015-01-30",1])==16085)
+result_data_emd<-result_data
+
+result_data_sma<-aaply(as.numeric(.indexDate(testdata["2011-01-01/2015-01-19",1])), 1, 
+function(x,ts,i,j){
+  start_date_id<-which(.indexDate(ts)==x)
+  end_date<-.indexDate(ts)[(start_date_id+i-1)]
+  if(!is.na(end_date))
+  {
+    start_end_str<-paste(as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),
+                         "/",
+                         as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),
+                         sep="")
+    start_str<-paste("/",as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+    print(start_end_str)
+    print(start_str)
+    ac_indicator(ts[start_end_str],ts[start_str],i,j)
+  }
+  
+  },testdata[,1],10,3)
+
+
+
+xxx_close_normalize<-(xxx[,"close"]-min(xxx[,"close"]))/(max(xxx[,"close"])-min(xxx[,"close"]))+1
+eemd_test<-Rlibeemd::eemd(xxx_close_normalize)
+
+xxx_sma_5<-SMA(xxx[,"close"],5)
+xxx_sma_5<-xxx_sma_5[!is.na(xxx_sma_5)]
+normalize_func(xxx_sma_5,max_sma5,min_sma5)
+xxx_sma_5_normalize<-(xxx_sma_5-min(xxx_sma_5))/(max(xxx_sma_5)-min(xxx_sma_5))+1
+xxx_sma_5_normalize<-cbind(Rlibeemd::eemd(xxx_sma_5_normalize),xxx_sma_5_normalize)
+xxx_sma_5_normalize_xts<-xts(xxx_sma_5_normalize,order.by=as.Date(.indexDate(xxx_sma_5)))  
+
+
+xxx_close<-cbind(xxx_close_normalize,Rlibeemd::eemd(xxx_close_normalize))
+xxx_close_xts<-xts(xxx_close,order.by=as.Date(.indexDate(xxx)))  
+
+
+result_data_eemd2<-aaply(as.numeric(.indexDate(xxx_close_xts["2014-01-01/2015-01-19",3])), 1, 
+function(x,ts,i,j){
+
+  start_date_id<-which(.indexDate(ts)==x)
+  end_date<-.indexDate(ts)[(start_date_id+i-1)]
+  if(!is.na(end_date))
+  {
+    start_end_str<-paste(as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),
+                         "/",
+                         as.Date(end_date,format="%Y-%m-%d",origin = "1970-01-01"),
+                         sep="")
+    start_str<-paste("/",as.Date(x,format="%Y-%m-%d",origin = "1970-01-01"),sep="")
+    print(start_end_str)
+    print(start_str)
+    ac_indicator(ts[start_end_str],ts[start_str],i,j)
+  }
+  
+},xxx_close_xts[,3],10,3)
+
+result_data<-ac_indicator(xxx_close_xts["2011-01-10/2015-01-30"],xxx_close_xts["/2011-01-01"],10,3)
+
+plot_data<-cbind(xxx["2011-01-17/2015-01-30","close"],result_data_emd)
+plot_data<-merge(testdata["2011-01-01/2011-01-30"],result_data,join="left")
+plot_data<-result_data[,c(1,6,7,8)]
+
+plot(plot_data["2014-10-17/2015-01-30",1],ylim=c(min(plot_data["2014-01-17/2015-01-30"],na.rm=T),max(plot_data["2014-01-17/2015-01-30"],na.rm=T)))
+lines(plot_data["2014-10-17/2015-01-30",2],col="red")
+lines(plot_data["2014-10-17/2015-01-30",3],col="blue")
+lines(plot_data["2014-10-17/2015-01-30",4],col="yellow")
+
+
+
+pred_close_price<-HS300_idx_ts["2008-01-15/2015-01-30"]
+pred_close_price$sell_signal<-((result_data_hs300_eemd_smooth[,1]>result_data_hs300_eemd_smooth[,2])&(result_data_hs300_eemd_smooth[,2]>result_data_hs300_eemd_smooth[,3]))
+pred_close_price$buy_signal<-((result_data_hs300_eemd_smooth[,1]<result_data_hs300_eemd_smooth[,2])&(result_data_hs300_eemd_smooth[,2]<result_data_hs300_eemd_smooth[,3]))
+pred_close_price$close<-pred_close_price$close/1000
+pred_close_price$high<-pred_close_price$high/1000
+pred_close_price$low<-pred_close_price$low/1000
+pred_close_price$open<-pred_close_price$open/1000
+
+
+result_data1<-plot_data[,(1:4)]
+result_data1$sell_signal<-((result_data1[,2]>result_data1[,3])&(result_data1[,3]>result_data1[,4]))
+
+result_data1$buy_signal<-((result_data1[,2]<result_data1[,3])&(result_data1[,3]<result_data1[,4]))
+
+chuquan<-as.Date(c("2014-06-10","2013-06-18",'2012-10-17'),format="%Y-%m-%d",origin = "1970-01-01")
+is_chuquan<-rep(1,length(chuquan))
+chuquan_ts<-xts(is_chuquan,order.by=chuquan)
+colnames(chuquan_ts)<-"chuquan"
+
+result_data1$sma<-SMA(result_data1[,"close"],5)
+result_data1$bias<-(result_data1[,"close"]-result_data1[,"sma"])/result_data1[,"sma"]*100
+result_data1<-merge(xxx["2011-01-17/2015-01-30"],sell_signal,join="left")
+result_data1<-merge(result_data1["2011-01-17/"],buy_signal,join="left")
+result_data1<-merge(result_data1["2011-01-17/"],chuquan_ts,join="left")
+result_data1$chuquan[is.na(result_data1$chuquan)]<-0
+result_data1<-result_data1[!result_data1$close==0]
+
+result_data1$buy_signal<-(result_data1$buy_signal==1)&(result_data1$bias < -3)
+
+
+1] "2012-01-11 00:00:00 ZSYH 9000 @ 0"
+[1] "2012-01-12 00:00:00 ZSYH -9000 @ 0"
+[1] "2012-01-17 00:00:00 ZSYH 9000 @ 0"
+
+
+result_data1["2012-01-11/2012-01-17"]
+xxx[!xxx$close==0]

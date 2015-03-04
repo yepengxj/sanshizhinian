7a2b0bbd4b36ffae59d906f0e3f3bf1b3a3c0695
yepengxj <yepeng_xj@163.com>
2015-03-04 23:54:26 +0800

    modified:   kpca_svm.R

--- kpca_svm.R
+++ kpca_svm.R
@@ -3,8 +3,98 @@ library(quantstrat)
 library(dtw)
 library(plyr)
 library("e1071")
-library("hht")
 library("kernlab")
+
+
+
+
+xts_shitf_days<-function(xts_data,shitf_days)
+{
+  if(shitf_days>0)
+  {
+    new_idx_date<-.indexDate(xts_data)[1:(length(.indexDate(xts_data))-shitf_days)]
+    
+    shitf_data<-as.data.frame(xts_data)[(1+shitf_days):length(.indexDate(xts_data)),]
+    
+    xts(shitf_data,order.by=(as.Date(new_idx_date,format="%Y-%m-%d",origin = "1970-01-01")))
+    
+  }
+  else
+  {
+    new_idx_date<-.indexDate(xts_data)
+    shitf_data<-as.data.frame(xts_data)
+    dim_data<-dim(shitf_data)
+    for(i in (1:abs(shitf_days)))
+    {
+      shitf_data<-rbind(rep(NA,dim_data[2]),shitf_data)
+    }
+    
+    xts(shitf_data[1:length(new_idx_date),],order.by=(as.Date(new_idx_date,format="%Y-%m-%d",origin = "1970-01-01")))
+    
+  }
+  
+}
+
+#=====EEMD趋势拟合测试
+
+#归一化
+normalize_func<-function(x)
+{
+  (x-min(x))/(max(x)-min(x))+1
+}
+
+denormalize_func<-function(x,max_x,min_x)
+{
+  (x-1)*(max_x-min_x)+min_x
+}
+
+
+svm_mse_fitness<-function(x,training,trainingTarget,testing,testingTarget){
+  #建立SVM
+  svm_model<-svm(x=training,y=trainingTarget,cost=x[1],gamma=x[2])
+  
+  #做预测
+  pred<-predict(svm_model,testing)
+  
+  #calc_fitness(MSE)
+  -sqrt(sum((testingTarget-pred)^2))/length(testingTarget)
+  
+}
+
+kpca_svm_ga_func<-function(ts,date_range,factor_col,prd_col)
+{
+  
+  model_data<-as.matrix(ts[,c(factor_col,prd_col)])
+  
+  #model_data_normalize<-cbind(t(aaply(model_data[,normalize_col],2,normalize_func)),model_data[,non_normalize_col] )
+  model_data_normalize<-t(aaply(model_data,2,normalize_func))
+  model_data_normalize_kpca<-pcv(kpca(~.,data=data.frame(model_data_normalize[,factor_col]),kernel="rbfdot",
+                                      kpar=list(sigma=0.2),features=2))
+  #train_set<-as.integer(nrow(model_data_normalize)*0.9)
+  train_set<-nrow(model_data_normalize_kpca)-2
+  training<-model_data_normalize_kpca[1:train_set,]
+  trainingTarget<-model_data_normalize[1:train_set,prd_col]
+  
+  
+  testing<-model_data_normalize_kpca[(train_set:nrow(model_data_normalize_kpca)),]
+  testingTarget<-model_data_normalize[(train_set:nrow(model_data_normalize)),prd_col]
+  
+  GA <- ga(type = "real-valued",
+           fitness = svm_mse_fitness, training, trainingTarget, testing, testingTarget,
+           min = c(0, 0), max = c(100, 100), popSize = 50,
+           maxiter = 50)
+  
+  svm_model<-svm(x=training,y=trainingTarget,cost=summary(GA)$solution[1,1],gamma=summary(GA)$solution[1,2])
+  print("svm_model")
+  predict_data<-predict(svm_model,testing)
+  print(date_range)
+  c(as.numeric(format(as.Date(strsplit(date_range,"/",fixed=T)[[1]][2]),"%Y%m%d")), 
+    as.numeric(denormalize_func(predict_data,max(model_data[,prd_col]), min(model_data[,prd_col]))),
+    as.numeric(denormalize_func(testingTarget,max(model_data[,prd_col]), min(model_data[,prd_col])))
+  )
+  
+  
+}
 
 #####准备数据from tdx
 hs300_idx<-read.csv("~/temp/0000300.csv",header=F,skip=1,encoding = "UTF-8")
@@ -70,12 +160,6 @@ HS300_idx_ts$log2high_low5<-sqrt(runSum(log2(HS300_idx_ts[,"high"]/HS300_idx_ts[
 HS300_idx_ts_kpca<-kpca(as.matrix(HS300_idx_ts[,c(-1,-2,-3,-4,-5)]),kernel="rbfdot")
 
 ##SVM
-kernel_type <- "radial"
-max_gamma <- -2
-min_gamma <- -6
-max_cost <- 3
-min_cost <- -6
-
 
 factor_col<-c("ema15_close", "ema15_last1day_close","ema15_last2day_close",
               "ema15_last3day_close", "ema10_close", "ema20_close","ema3_close",
@@ -104,93 +188,4 @@ pred_result<-aaply(as.numeric(.indexDate(HS300_idx_ts["2015-02-02/2015-02-03",])
                      
                    },HS300_idx_ts["2014-01-01/",],factor_col,prd_col)
 
-
-kpca_svm_ga_func<-function(ts,date_range,factor_col,prd_col)
-{
-  
-  model_data<-as.matrix(ts[,c(factor_col,prd_col)])
-  
-  #model_data_normalize<-cbind(t(aaply(model_data[,normalize_col],2,normalize_func)),model_data[,non_normalize_col] )
-  model_data_normalize<-t(aaply(model_data,2,normalize_func))
-  model_data_normalize_kpca<-pcv(kpca(~.,data=data.frame(model_data_normalize[,factor_col]),kernel="rbfdot",
-                                      kpar=list(sigma=0.2),features=2))
-  #train_set<-as.integer(nrow(model_data_normalize)*0.9)
-  train_set<-nrow(model_data_normalize_kpca)-2
-  training<-model_data_normalize_kpca[1:train_set,]
-  trainingTarget<-model_data_normalize[1:train_set,prd_col]
-  
-  
-  testing<-model_data_normalize_kpca[(train_set:nrow(model_data_normalize_kpca)),]
-  testingTarget<-model_data_normalize[(train_set:nrow(model_data_normalize)),prd_col]
-  
-  
-  
-  svm_mse_fitness<-function(x,training,trainingTarget,testing,testingTarget){
-    #建立SVM
-    svm_model<-svm(x=training,y=trainingTarget,cost=x[1],gamma=x[2])
-    
-    #做预测
-    pred<-predict(svm_model,testing)
-    
-    #calc_fitness(MSE)
-    -sqrt(sum((testingTarget-pred)^2))/length(testingTarget)
-    
-  }
-  
-  GA <- ga(type = "real-valued",
-           fitness = svm_mse_fitness, training, trainingTarget, testing, testingTarget,
-           min = c(0, 0), max = c(100, 100), popSize = 50,
-           maxiter = 50)
-  
-  svm_model<-svm(x=training,y=trainingTarget,cost=summary(GA)$solution[1,1],gamma=summary(GA)$solution[1,2])
-  
-  predict_data<-predict(svm_model,testing)
-  
-  c( as.numeric(denormalize_func(predict_data,max(model_data[,prd_col]), min(model_data[,prd_col]))),
-     as.numeric(denormalize_func(testingTarget,max(model_data[,prd_col]), min(model_data[,prd_col]))),
-     date_range)
-
-
-}
-
-
-xts_shitf_days<-function(xts_data,shitf_days)
-{
-  if(shitf_days>0)
-  {
-    new_idx_date<-.indexDate(xts_data)[1:(length(.indexDate(xts_data))-shitf_days)]
-    
-    shitf_data<-as.data.frame(xts_data)[(1+shitf_days):length(.indexDate(xts_data)),]
-    
-    xts(shitf_data,order.by=(as.Date(new_idx_date,format="%Y-%m-%d",origin = "1970-01-01")))
-    
-  }
-  else
-  {
-    new_idx_date<-.indexDate(xts_data)
-    shitf_data<-as.data.frame(xts_data)
-    dim_data<-dim(shitf_data)
-    for(i in (1:abs(shitf_days)))
-    {
-      shitf_data<-rbind(rep(NA,dim_data[2]),shitf_data)
-    }
-    
-    xts(shitf_data[1:length(new_idx_date),],order.by=(as.Date(new_idx_date,format="%Y-%m-%d",origin = "1970-01-01")))
-    
-  }
-  
-}
-
-#=====EEMD趋势拟合测试
-
-#归一化
-normalize_func<-function(x)
-{
-  (x-min(x))/(max(x)-min(x))+1
-}
-
-denormalize_func<-function(x,max_x,min_x)
-{
-  (x-1)*(max_x-min_x)+min_x
-}
-
+write.csv(pred_result, file = "~/temp/kpca_svm_pred_result.txt",row.names=F)

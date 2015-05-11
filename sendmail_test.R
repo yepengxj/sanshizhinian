3c2992c6df52877c85c000eaa2aa78646af3136f
yepengxj <yepeng_xj@163.com>
2015-05-11 11:04:19 +0800

    modified:   sendmail_test.R

--- sendmail_test.R
+++ sendmail_test.R
@@ -1,12 +1,20 @@
 library("mailR")
 library(XML)
-tt<-readHTMLTable(htmlParse("http://abcfund.cn/data/realdata.php?p=95",encoding="GBK"), header = T)[[1]]
+
+base_data<-htmlParse("http://abcfund.cn/data/realdata.php?p=95",encoding="GBK")
+
+data<-getNodeSet(base_data,"//body/data/table/tbody/tr/td/span")
+
+removeNodes(data)
+
+tt<-readHTMLTable(base_data, header = T)[[1]]
 test_data<-tt[order(-as.numeric(sub("%","",tt[,23])),na.last=T) ,c(1,2,9,16,22,23)]
 test_data<-test_data[test_data[,6]!="-",]
 
 html_content<-"<html><table>"
 
-html_content<-paste(html_content,"<tr>",sep = "")
+
+html_content<-paste(html_content,"<tr><td>溢价/折价</td>",sep = "")
 for(head_colname in colnames(test_data))
 {
   html_content<-paste(html_content,sprintf("<td>%s</td>",head_colname),sep = "")
@@ -14,10 +22,17 @@ for(head_colname in colnames(test_data))
 html_content<-paste(html_content,"</tr>",sep = "")
 
 top5_data<-head(test_data,5)
+#html_content<-paste(html_content,"<tr><td>溢价top5</td></tr>",sep = "")
 for(i in 1:dim(top5_data)[1])
 {
-  
-  html_content<-paste(html_content,"<tr>",sep = "")
+  if( i == 1)
+  {
+    html_content<-paste(html_content,"<tr><td rowspan=5>溢价top5</td>",sep = "")
+  }
+  else
+  {
+    html_content<-paste(html_content,"<tr>",sep = "")
+  }
   for(j in 1:dim(top5_data)[2])
   {
     html_content<-paste(html_content,sprintf("<td>%s</td>",top5_data[i,j]),sep = "")
@@ -26,10 +41,18 @@ for(i in 1:dim(top5_data)[1])
 }
 
 bot5_data<-tail(test_data,5)
+
 for(i in 1:dim(bot5_data)[1])
 {
   
-  html_content<-paste(html_content,"<tr>",sep = "")
+  if( i == 1)
+  {
+    html_content<-paste(html_content,"<tr><td rowspan=5>折价top5</td>",sep = "")
+  }
+  else
+  {
+    html_content<-paste(html_content,"<tr>",sep = "")
+  }
   for(j in 1:dim(bot5_data)[2])
   {
     html_content<-paste(html_content,sprintf("<td>%s</td>",bot5_data[i,j]),sep = "")
@@ -48,4 +71,3 @@ send.mail(from = "13999806237@139.com",
           smtp = list(host.name = "smtp.139.com", port = 25, user.name = "", passwd = "", ssl = F),
           authenticate = TRUE,
           send = TRUE)
-
